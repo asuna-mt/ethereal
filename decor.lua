14998922a4ee41a9752ed64dc2070f99ab380ca1
EmptyStar <gitmail@emptystar.email>
2025-06-18 23:27:11 -0500

    Remove thin ice from mapgen

--- decor.lua
+++ decor.lua
@@ -12,14 +12,6 @@ local function register_decoration(enabled, def)
 	core.register_decoration(def)
 end
 
--- thin ice
-
-register_decoration(ethereal.frost, {
-	place_on = {"default:silver_sand"},
-	fill_ratio = 1.0, y_min = 0, y_max = 0,
-	decoration = "ethereal:thin_ice", place_offset_y = 1,
-	biomes = {"frost_ocean"} })
-
 -- water pools in swamp areas
 
 register_decoration(1, {
